cd2d16b66a4eba6f8555e1bf15e5701dee1a9231
Steven Mikhu <StevenMikhu@gmail.com>
2022-02-16 12:23:21 -0500

    updated styling

--- src/components/Page2/Page2.js
+++ src/components/Page2/Page2.js
@@ -8,41 +8,42 @@ function Page2(props) {
             <div className="grid grid-item7">
                 <h3><span className="seven">7. CONNECTIONS</span></h3>
                 <div className="bubble bubble-bottom-left">
-                <p><span className="red">GROUP AFFILIATION - </span>{props.characters.connections['group-affiliation']}</p>
+                <p><span className="red">GROUP AFFILIATION - </span>{props.characters?.connections?.['group-affiliation']}</p>
                 <br></br>
-                <p><span className="red">RELATIVES - </span>{props.characters.connections['relatives']}</p>
+                <p><span className="red">RELATIVES - </span>{props.characters?.connections?.['relatives']}</p>
                 </div>
             </div>
             <div className="grid grid-item8">
                 <h3><span className="red">8. GENERAL INFORMATION</span></h3>
                 <div className="gen-info">
-                    <p><span className="red">Full Name - </span>{props.characters.biography['full-name']}</p>
-                    <p><span className="red">Place of birth - </span>{props.characters.biography['place-of-birth']}</p>
-                    <p><span className="red">Alignment - </span>{props.characters.biography.alignment}</p>
-                    <p><span className="red">Base - </span>{props.characters.work.base}</p>
-                    <p><span className="red">Occupation - </span>{props.characters.work.occupation}</p>
-                    <p><span className="red">Alter Egos - </span>{props.characters.biography['alter-egos']}</p>
-                    <p><span className="red">First Comic Appearance - </span>{props.characters.biography['first-appearance']}</p>
+                    <p><span className="red">Full Name - </span>{props.characters?.biography?.['full-name']}</p>
+                    <p><span className="red">Place of birth - </span>{props.characters?.biography?.['place-of-birth']}</p>
+                    <p><span className="red">Alignment - </span>{props.characters?.biography?.alignment}</p>
+                    <p><span className="red">Base - </span>{props.characters?.work?.base}</p>
+                    <p><span className="red">Occupation - </span>{props.characters?.work?.occupation}</p>
+                    <p><span className="red">Alter Egos - </span>{props.characters?.biography?.['alter-egos']}</p>
+                    <p><span className="red">First Comic Appearance - </span>{props.characters?.biography?.['first-appearance']}</p>
                 </div>
             </div>
             <div className="grid grid-item9">
                 <h3><span className="seven">9. ALIASES</span></h3>
                 <div className="aliases bubble2 bubble-bottom-right">
-                    <p>{props.characters.biography.aliases[0]}</p>
-                    <p>{props.characters.biography.aliases[1]}</p>
-                    <p>{props.characters.biography.aliases[2]}</p>
-                    <p>{props.characters.biography.aliases[3]}</p>
-                    <p>{props.characters.biography.aliases[4]}</p>
-                    <p>{props.characters.biography.aliases[5]}</p>
+                    <h4 className="red">Aliases - </h4>
+                    {props.characters?.biography?.aliases[0] ? <p>-{props.characters?.biography?.aliases[0]}</p> : null}
+                    {props.characters?.biography?.aliases[1] ? <p>-{props.characters?.biography?.aliases[1]}</p> : null}
+                    {props.characters?.biography?.aliases[2] ? <p>-{props.characters?.biography?.aliases[2]}</p> : null}
+                    {props.characters?.biography?.aliases[3] ? <p>-{props.characters?.biography?.aliases[3]}</p> : null}
+                    {props.characters?.biography?.aliases[4] ? <p>-{props.characters?.biography?.aliases[4]}</p> : null}
+                    {props.characters?.biography?.aliases[5] ? <p>-{props.characters?.biography?.aliases[5]}</p> : null}
                 </div>
             </div>
             <div className="grid grid-item10">
                 <h3><span className="seven">10. APPEARANCE</span></h3>
                 <div className="appearance">
-                    <p>Eye Color - {props.characters.appearance?.['eye-color']}</p>
-                    <p>Hair Color - {props.characters.appearance?.['hair-color']}</p>
-                    <p>Height - {props.characters.appearance?.height[0]}</p>
-                    <p>Weight - {props.characters.appearance?.weight[0]}</p>
+                {props.characters?.appearance?.['eye-color'] ? <p>Eye Color - {props.characters?.appearance?.['eye-color']}</p> : null}
+                {props.characters?.appearance?.['hair-color'] ? <p>Hair Color - {props.characters?.appearance?.['hair-color']}</p> : null}
+                {props.characters?.appearance?.height[0] ? <p>Height - {props.characters?.appearance?.height[0]}</p> : null}
+                {props.characters?.appearance?.weight[0] ? <p>Weight - {props.characters?.appearance?.weight[0]}</p> : null}
                 </div>
             </div>
             <div className="grid grid-item11">
@@ -51,7 +52,7 @@ function Page2(props) {
             <div className="grid grid-item12">
                 <h3><span className="seven">12. PUBLISHER</span></h3>
                 <div className="publisher">
-                    <p>{props.characters.biography.publisher}</p>
+                    <p>{props.characters?.biography?.publisher}</p>
                 </div>
             </div>
         </div>
